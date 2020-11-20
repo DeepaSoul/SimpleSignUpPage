009cb4bdf96b459214e944585d3fc18f76f7e88d
DeepaSoul <42563346+DeepaSoul@users.noreply.github.com>
2020-11-20 12:37:19 +0200

    Update app.js
    Added input field validation

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const sign_up_submit = document.querySelector("#sign-up-submit");
 const sign_in_submit = document.querySelector("#sign-in-submit");
 const container = document.querySelector(".container");
-var ErrorLog = ""
 
 //Add the Class sign up so we can transition to the sign up screen
 sign_up_btn.addEventListener("click", (e) => {
@@ -18,28 +17,44 @@ sign_in_btn.addEventListener("click", (e) => {
 
 //Validate form fields
 sign_up_submit.addEventListener("click", (e) => {
-  e.preventDefault()
-  var x = document.forms["signUpForm"]["phoneNum"].value;
-  console.log(x.match(/[^A-Za-z0-9\-_]/))
-
-  // if(){
-
-  // }
-
+  e.preventDefault();
+  if(!validateField("tel", document.forms["signUpForm"]["phoneNum"].value, "phoneNum").success) document.querySelector(".phoneNum").classList.add("error");
+if(!validateField("tel", document.forms["signUpForm"]["fname"].value, "Name").success) document.querySelector(".fname").classList.add("error");
+if(!validateField("tel", document.forms["signUpForm"]["lname"].value, "Last Name").success) document.querySelector(".lname").classList.add("error");
+if(!validateField("tel", document.forms["signUpForm"]["email"].value, "email").success) document.querySelector(".email").classList.add("error");
 });
 
 sign_in_submit.addEventListener("click", (e) => {
   e.preventDefault()
 });
 
+//Field validation function, with error log message.
 validateField = (type, value, name) => {
+var result: {
+success: true,
+msg: "Data Valid."
+}
   if(type == "email"){
     // [a-z]
+    if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
+result = {
+success: false,
+msg: "Email Structure incorrect."
+}
+    }
   }else if(type == "tel"){
-
-    value.includes()
+  if(!(value.match(/^\d{10}$/)){
+    result = {
+    success: false,
+    msg: "Phone not correct Structure incorrect."
+    }
+    }
 
   }else{
-    value === "" ? ErrorLog + `${name} field must be field in.` : null
+    value === "" ? 
+    result = {
+        success: false,
+        msg: `${name} field must be field in.`
+    } : null
   }
 }
